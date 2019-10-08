2a6a149347d68248a7085de8a4869333b5bedad2
Nikola Maeda <nmaeda@sfu.ca>
2019-10-07 19:59:11 -0700

    Indentation Fix

--- assign2_part2/calc.c
+++ assign2_part2/calc.c
@@ -19,20 +19,20 @@ int num_ops;
    characters).  */
 char *int2string(int i, char *s)
 {
-    sprintf(s, "%d", i);
-    return s;
+	sprintf(s, "%d", i);
+	return s;
 }
 
 /* string2int just calls atoi() */
 int string2int(const char *s)
 {
-    return atoi(s);
+	return atoi(s);
 }
 
 /* isNumeric just calls isdigit() */
 int isNumeric(char c)
 {
-    return isdigit(c);
+	return isdigit(c);
 }
 
 /* End utility functions */
@@ -40,15 +40,15 @@ int isNumeric(char c)
 
 void printErrorAndExit(char *msg)
 {
-    msg = msg ? msg : "An unspecified error occured!";
-    fprintf(stderr, "%s\n", msg);
-    exit(EXIT_FAILURE);
+	msg = msg ? msg : "An unspecified error occured!";
+	fprintf(stderr, "%s\n", msg);
+	exit(EXIT_FAILURE);
 }
 
 int timeToFinish()
 {
-    /* be careful: timeToFinish() also accesses buffer */
-    return buffer[0] == '.';
+	/* be careful: timeToFinish() also accesses buffer */
+	return buffer[0] == '.';
 }
 
 /* Looks for an addition symbol "+" surrounded by two numbers, e.g. "5+6"
@@ -57,82 +57,82 @@ int timeToFinish()
    to worry about associativity! */
 void *adder(void *arg)
 {
-    int bufferlen;
-    int value1, value2;
-    int startOffset, remainderOffset;
-    int i;
-    printf("ADDER\n");
+	int bufferlen;
+	int value1, value2;
+	int startOffset, remainderOffset;
+	int i;
+	printf("ADDER\n");
 
-    while (1) 
-    {
-	startOffset = remainderOffset = -1;
-	value1 = value2 = -1;
-
-	if (timeToFinish()) 
+	while (1) 
 	{
-	    return NULL;
-	}	
+		startOffset = remainderOffset = -1;
+		value1 = value2 = -1;
 
-	/* storing this prevents having to recalculate it in the loop */
-	bufferlen = strlen(buffer);
-	char val1[bufferlen] ,val2[bufferlen];
-	int index1 = 0,index2 = 0;
-	int afterPlus = 0;
-	for (i = 0; i < bufferlen; i++)
-       	{
-	    // do we have value1 already?  If not, is this a "naked" number?
-	    // if we do, is the next character after it a '+'?
-	    // if so, is the next one a "naked" number?
-	    // once we have value1, value2 and start and end offsets of the
-	    // expression in buffer, replace it with v1+v2
-	    if(isNumeric(buffer[i]))
-	    {
-		    if(startOffset == -1)
-		    {
-			    startOffset = i;
-		    }
-		    //It's a numerical value and before +
-		    if(afterPlus)
-		    {
-			    val2[index2] = buffer[i];
-			    index2 ++;
-		    }
-		    //It's a numerical value and after + 
-		    else
-		    {
-			    val1[index1] = buffer[i];
-			    index1 ++;
-		    }
-	    }
-	    else 
-	    {
-		    if(index1 != 0 && index2 != 0)
-		    {
-			    remainderOffset = i-1;
-			    val1[index1+1] = '\0';
-			    val2[index2+1] = '\0';
-			    value1 = string2int(val1);
-			    value2 = string2int(val2);
-			    int total = value1 + value2;
-			    char number[20];
-			    int2string(total,number);
-			    strcpy(buffer,&buffer[i]);
-			    strcat(number,buffer);
-			    index1 = index2 = 0;
-		    }
-		    else if(index2 == 0)
-		    {
+		if (timeToFinish()) 
+		{
+			return NULL;
+		}	
 
-		    }
-		    else if(buffer[i] == '+')
-		    {
-			    afterPlus = 1;
-		    }	   
-	// something missing?
-	   }
-    	}	
-    //return NULL;
-   }
+		/* storing this prevents having to recalculate it in the loop */
+		bufferlen = strlen(buffer);
+		char val1[bufferlen] ,val2[bufferlen];
+		int index1 = 0,index2 = 0;
+		int afterPlus = 0;
+		for (i = 0; i < bufferlen; i++)
+		{
+			// do we have value1 already?  If not, is this a "naked" number?
+			// if we do, is the next character after it a '+'?
+			// if so, is the next one a "naked" number?
+			// once we have value1, value2 and start and end offsets of the
+			// expression in buffer, replace it with v1+v2
+			if(isNumeric(buffer[i]))
+			{
+				if(startOffset == -1)
+				{
+					startOffset = i;
+				}
+				//It's a numerical value and before +
+				if(afterPlus)
+				{
+					val2[index2] = buffer[i];
+					index2 ++;
+				}
+				//It's a numerical value and after + 
+				else
+				{
+					val1[index1] = buffer[i];
+					index1 ++;
+				}
+			}
+			else 
+			{
+				if(index1 != 0 && index2 != 0)
+				{
+					remainderOffset = i-1;
+					val1[index1+1] = '\0';
+					val2[index2+1] = '\0';
+					value1 = string2int(val1);
+					value2 = string2int(val2);
+					int total = value1 + value2;
+					char number[20];
+					int2string(total,number);
+					strcpy(buffer,&buffer[i]);
+					strcat(number,buffer);
+					index1 = index2 = 0;
+				}
+				else if(index2 == 0)
+				{
+
+				}
+				else if(buffer[i] == '+')
+				{
+					afterPlus = 1;
+				}	   
+				// something missing?
+			}
+		}	
+		//return NULL;
+	}
 }
 /* Looks for a multiplication symbol "*" surrounded by two numbers, e.g.
    "5*6" and, if found, multiplies the two numbers and replaces the
@@ -143,15 +143,15 @@ void *multiplier(void *arg)
 	int bufferlen;
 	int value1, value2;
 	int startOffset, remainderOffset;
-        int i;
-    	printf("ADDER\n");
-    	while (1) 
-    	{
+	int i;
+	printf("ADDER\n");
+	while (1) 
+	{
 		startOffset = remainderOffset = -1;
 		value1 = value2 = -1;
 		if (timeToFinish()) 
 		{
-		    return NULL;
+			return NULL;
 		}	
 		/* storing this prevents having to recalculate it in the loop */
 		bufferlen = strlen(buffer);
@@ -160,59 +160,59 @@ void *multiplier(void *arg)
 		int afterPlus = 0;
 		for (i = 0; i < bufferlen; i++)
 		{
-		    // do we have value1 already?  If not, is this a "naked" number?
-		    // if we do, is the next character after it a '+'?
-		    // if so, is the next one a "naked" number?
-		    // once we have value1, value2 and start and end offsets of the
-		    // expression in buffer, replace it with v1+v2
-		    if(isNumeric(buffer[i]))
-		    {
-			    if(startOffset == -1)
-			    {
-				    startOffset = i;
-			    }
-			    //It's a numerical value and before +
-			    if(afterPlus)
-			    {
-				    val2[index2] = buffer[i];
-				    index2 ++;
-			    }
-			    //It's a numerical value and after + 
-			    else
-			    {
-				    val1[index1] = buffer[i];
-				    index1 ++;
-			    }
-		    }
-		    else 
-		    {
-			    if(index1 != 0 && index2 != 0)
-			    {
-				    remainderOffset = i-1;
-				    val1[index1+1] = '\0';
-				    val2[index2+1] = '\0';
-				    value1 = string2int(val1);
-				    value2 = string2int(val2);
-				    int total = value1 + value2;
-				    char number[20];
-				    int2string(total,number);
-				    strcpy(buffer,&buffer[i]);
-				    strcat(number,buffer);
-				    index1 = index2 = 0;
-			    }
-			    else if(index2 == 0)
-			    {
+			// do we have value1 already?  If not, is this a "naked" number?
+			// if we do, is the next character after it a '+'?
+			// if so, is the next one a "naked" number?
+			// once we have value1, value2 and start and end offsets of the
+			// expression in buffer, replace it with v1+v2
+			if(isNumeric(buffer[i]))
+			{
+				if(startOffset == -1)
+				{
+					startOffset = i;
+				}
+				//It's a numerical value and before +
+				if(afterPlus)
+				{
+					val2[index2] = buffer[i];
+					index2 ++;
+				}
+				//It's a numerical value and after + 
+				else
+				{
+					val1[index1] = buffer[i];
+					index1 ++;
+				}
+			}
+			else 
+			{
+				if(index1 != 0 && index2 != 0)
+				{
+					remainderOffset = i-1;
+					val1[index1+1] = '\0';
+					val2[index2+1] = '\0';
+					value1 = string2int(val1);
+					value2 = string2int(val2);
+					int total = value1 + value2;
+					char number[20];
+					int2string(total,number);
+					strcpy(buffer,&buffer[i]);
+					strcat(number,buffer);
+					index1 = index2 = 0;
+				}
+				else if(index2 == 0)
+				{
 
-			    }
-			    else if(buffer[i] == '+')
-			    {
-				    afterPlus = 1;
-			    }	   
-		            // something missing?
-		   }
-    		}	
-    //return NULL;
-   }
+				}
+				else if(buffer[i] == '+')
+				{
+					afterPlus = 1;
+				}	   
+				// something missing?
+			}
+		}	
+		//return NULL;
+	}
 }
 
 
@@ -221,66 +221,65 @@ void *multiplier(void *arg)
    only the surrounded number. */
 void *degrouper(void *arg)
 {
-    int bufferlen;
-    int i;
-    printf("DEGROUPER\n");
-    //return NULL; /* remove this line */
+	int bufferlen;
+	int i;
+	printf("DEGROUPER\n");
+	//return NULL; /* remove this line */
+	while (1)
+	{
+		if (timeToFinish())
+		{
+			return NULL;
+		}
 
-    while (1) {
+		/* storing this prevents having to recalculate it in the loop */
+		bufferlen = strlen(buffer);
+		int start, end;
+		int afterBracket,afterStoring,index = 0;
+		char value [bufferlen];
+		for (i = 0; i < bufferlen; i++) 
+		{
+			// check for '(' followed by a naked number followed by ')'
+			// remove ')' by shifting the tail end of the expression
+			// remove '(' by shifting the beginning of the expression
+			if(buffer[i] == '(')
+			{
+				afterBracket = 1;
+				continue;
+			}
+			else if(isNumeric(buffer[i]))
+			{
+				if(afterBracket)
+				{
+					value[index] = buffer[i];
+					index ++; 
+					afterStoring = 1;
+					start = i;
+				}
+			} 
+			else if(buffer[i]==')')
+			{
+				if(afterStoring)
+				{
+					end = i;
+					afterStoring = 0;
+					afterBracket = 0;
+					strcpy(buffer[start], &value);
+				}
 
-	if (timeToFinish()) {
-	    return NULL;
+			}
+			else
+			{
+				afterBracket = 0;
+				afterStoring = 0;
+
+			}
+
+
+		}
+
+		// something missing?
 	}
-
-	/* storing this prevents having to recalculate it in the loop */
-	bufferlen = strlen(buffer);
-	int start, end;
-	int afterBracket,afterStoring,index = 0;
-	char value [bufferlen];
-	for (i = 0; i < bufferlen; i++) {
-	    // check for '(' followed by a naked number followed by ')'
-	    // remove ')' by shifting the tail end of the expression
-	    // remove '(' by shifting the beginning of the expression
-	    if(buffer[i] == '(')
-	    {
-		    afterBracket = 1;
-		    continue;
-	    }
-	    else if(isNumeric(buffer[i]))
-	    {
-		    if(afterBracket)
-		    {
-			    //while(isNumerical(buffer[i]))
-
-			    value[index] = buffer[i];
-			    index ++; 
-			    afterStoring = 1;
-			    start = i;
-		    }
-	    } 
-	    else if(buffer[i]==')')
-	    {
-		    if(afterStoring)
-		    {
-			    end = i;
-			    afterStoring = 0;
-			    afterBracket = 0;
-			    strcpy(buffer[start], &value);
-		    }
-
-	    }
-	    else
-	    {
-		    afterBracket = 0;
-		    afterStoring = 0;
-
-	    }
-
-	
-	}
-
-	// something missing?
-    }
 }
 
 
@@ -291,112 +290,111 @@ void *degrouper(void *arg)
    proceed on the next (if available). */
 void *sentinel(void *arg)
 {
-    char numberBuffer[20];
-    int bufferlen;
-    int i;
-    printf("SENTINEL\n");
-    //return NULL; /* remove this line */
+	char numberBuffer[20];
+	int bufferlen;
+	int i;
+	printf("SENTINEL\n");
+	//return NULL; /* remove this line */
 
-    while (1) {
+	while (1) {
 
-	if (timeToFinish()) {
-	    return NULL;
-	}
-	while(buffer[0]==';')
-	{}
-
-	/* storing this prevents having to recalculate it in the loop */
-	bufferlen = strlen(buffer);
-
-	for (i = 0; i < bufferlen; i++) {
-	    if (buffer[i] == ';') {
-		if (i == 0) {
-		    printErrorAndExit("Sentinel found empty expression!");
-		} else {
-		    /* null terminate the string */
-		    numberBuffer[i] = '\0';
-		    /* print out the number we've found */
-		    fprintf(stdout, "%s\n", numberBuffer);
-		    /* shift the remainder of the string to the left */
-		    strcpy(buffer, &buffer[i + 1]);
-		    break;
+		if (timeToFinish()) {
+			return NULL;
 		}
-	    } else if (!isNumeric(buffer[i])) {
-		break;
-	    } else {
-		numberBuffer[i] = buffer[i];
-	    }
+		while(buffer[0]==';')
+		{}
+
+		/* storing this prevents having to recalculate it in the loop */
+		bufferlen = strlen(buffer);
+
+		for (i = 0; i < bufferlen; i++) {
+			if (buffer[i] == ';') {
+				if (i == 0) {
+					printErrorAndExit("Sentinel found empty expression!");
+				} else {
+					/* null terminate the string */
+					numberBuffer[i] = '\0';
+					/* print out the number we've found */
+					fprintf(stdout, "%s\n", numberBuffer);
+					/* shift the remainder of the string to the left */
+					strcpy(buffer, &buffer[i + 1]);
+					break;
+				}
+			} else if (!isNumeric(buffer[i])) {
+				break;
+			} else {
+				numberBuffer[i] = buffer[i];
+			}
+		}
+		sched_yield();
+		// something missing?
 	}
-	sched_yield();
-	// something missing?
-    }
 }
 
 /* reader reads in lines of input from stdin and writes them to the
    buffer */
 void *reader(void *arg)
 {
-    while (1) {
-	char tBuffer[100];
-	int currentlen;
-	int newlen;
-	int free;
-	printf("READER\n");
-	fgets(tBuffer, sizeof(tBuffer), stdin);
-	
-	/* Sychronization bugs in remainder of function need to be fixed */
+	while (1) {
+		char tBuffer[100];
+		int currentlen;
+		int newlen;
+		int free;
+		printf("READER\n");
+		fgets(tBuffer, sizeof(tBuffer), stdin);
 
-	newlen = strlen(tBuffer);
-	currentlen = strlen(buffer);
+		/* Sychronization bugs in remainder of function need to be fixed */
 
-	/* if tBuffer comes back with a newline from fgets, remove it */
-	if (tBuffer[newlen - 1] == '\n') {
-	    /* shift null terminator left */
-	    tBuffer[newlen - 1] = tBuffer[newlen];
-	    newlen--;
+		newlen = strlen(tBuffer);
+		currentlen = strlen(buffer);
+
+		/* if tBuffer comes back with a newline from fgets, remove it */
+		if (tBuffer[newlen - 1] == '\n') 
+		{
+			/* shift null terminator left */
+			tBuffer[newlen - 1] = tBuffer[newlen];
+			newlen--;
+		}
+
+		/* -1 for null terminator, -1 for ; separator */
+		free = sizeof(buffer) - currentlen - 2;
+
+		while (free < newlen)
+		{
+			// spinwaiting
+		}
+
+		/* we can add another expression now */
+		strcat(buffer, tBuffer);
+		strcat(buffer, ";");
+
+		/* Stop when user enters '.' */
+		if (tBuffer[0] == '.') {
+			return NULL;
+		}
 	}
-
-	/* -1 for null terminator, -1 for ; separator */
-	free = sizeof(buffer) - currentlen - 2;
-
-	while (free < newlen) {
-		// spinwaiting
-	}
-
-	/* we can add another expression now */
-	strcat(buffer, tBuffer);
-	strcat(buffer, ";");
-
-	/* Stop when user enters '.' */
-	if (tBuffer[0] == '.') {
-	    return NULL;
-	}
-    }
 }
 
 
 /* Where it all begins */
 int smp3_main(int argc, char **argv)
 {
-    void *arg = 0;		/* dummy value */
+	void *arg = 0;		/* dummy value */
 
-    /* let's create our threads */
-    if (pthread_create(&multiplierThread, NULL, multiplier, arg)
-	|| pthread_create(&adderThread, NULL, adder, arg)
-	|| pthread_create(&degrouperThread, NULL, degrouper, arg)
-	|| pthread_create(&sentinelThread, NULL, sentinel, arg)
-	|| pthread_create(&readerThread, NULL, reader, arg)) {
-	printErrorAndExit("Failed trying to create threads");
-    }
+	/* let's create our threads */
+	if (pthread_create(&multiplierThread, NULL, multiplier, arg) || pthread_create(&adderThread, NULL, adder, arg)|| pthread_create(&degrouperThread, NULL, degrouper, arg)|| pthread_create(&sentinelThread, NULL, sentinel, arg)|| pthread_create(&readerThread, NULL, reader, arg))
+       	{
+		printErrorAndExit("Failed trying to create threads");
+	}
 
-    /* you need to join one of these threads... but which one? */
-    pthread_detach(multiplierThread);
-    pthread_detach(adderThread);
-    pthread_detach(degrouperThread);
-    pthread_detach(readerThread);	
-    pthread_join(sentinelThread,NULL);
-    
-    /* everything is finished, print out the number of operations performed */
-    fprintf(stdout, "Performed a total of %d operations\n", num_ops);
-    return EXIT_SUCCESS;
+	/* you need to join one of these threads... but which one? */
+	pthread_detach(multiplierThread);
+	pthread_detach(adderThread);
+	pthread_detach(degrouperThread);
+	pthread_detach(readerThread);	
+	pthread_join(sentinelThread,NULL);
+
+	/* everything is finished, print out the number of operations performed */
+	fprintf(stdout, "Performed a total of %d operations\n", num_ops);
+	return EXIT_SUCCESS;
 }
